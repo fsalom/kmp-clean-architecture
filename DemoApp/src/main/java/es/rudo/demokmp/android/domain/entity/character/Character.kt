662e1c166da871b0704d6f3ef19b9dce28f1e120
Fernando Salom <fer@rudo.es>
2024-06-09 19:50:30 +0200

    Add character entity

---
+++ DemoApp/src/main/java/es/rudo/demokmp/android/domain/entity/character/Character.kt
@@ -0,0 +1,10 @@
+package es.rudo.demokmp.android.domain.entity.character
+
+data class Character(
+    val id: Int = 0,
+    val name: String = "",
+    val status: String = "",
+    val species: String = "",
+    val image: String = "",
+    val episodes: List<String> = listOf()
+)
