ad36c2f0c56c9a9e0a24fb8cbb2021e98d460514
Fernando Salom <fer@rudo.es>
2024-08-15 19:24:37 +0200

    Add new greetings example

--- shared/src/commonMain/kotlin/es/rudo/demokmp/Greeting.kt
+++ shared/src/commonMain/kotlin/es/rudo/demokmp/Greeting.kt
@@ -1,9 +1,20 @@
 package es.rudo.demokmp
 
+import com.example.clean.data.repository.CharacterRepositoryImpl
+import es.rudo.demokmp.data.datasource.character.RickAndMorty.CharacterRemoteDataSourceImpl
+import es.rudo.demokmp.domain.usecase.character.CharacterUseCaseImpl
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        return "Hello, ${platform.name}! this is the result: ${getNumber()}"
+    }
+
+    fun getNumber(): Int {
+        val datasource = CharacterRemoteDataSourceImpl()
+        val repository = CharacterRepositoryImpl(dataSource = datasource)
+        val useCase = CharacterUseCaseImpl(repository = repository)
+        return useCase.getNumber()
     }
 }
